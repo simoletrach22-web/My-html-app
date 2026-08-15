4c8e79f0484b4035d98a167b4811c55ead271c86
simoletrach22-web <simoletrach22@gmail.com>
2026-08-15 22:28:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,20 @@
-const CACHE_NAME = "zombie-game-v1";
-const urlsToCache = [
-"/",
-"/index.html",
-"/manifest.json",
-"/icon-192.png",
-"/icon-512.png"
-];
+    const CACHE = 'zombie-v1';
+    const FILES = [
+      '/',
+      '/index.html',
+      '/manifest.json',
+      '/icon-192.png',
+      '/icon-512.png'
+    ];
 
-// مني يتصطب
-self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache => {
-return cache.addAll(urlsToCache);
-})
-);
-});
+    self.addEventListener('install', e => {
+      e.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(FILES))
+      );
+    });
 
-// مني يطلب شي حاجة
-self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request).then(response => {
-return response || fetch(event.request);
-})
-);
-});
+    self.addEventListener('fetch', e => {
+      e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+      );
+    });
