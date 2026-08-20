cdc2db714a20f941d991ca388bdc1eccf8bed934
simoletrach22-web <simoletrach22@gmail.com>
2026-08-20 01:41:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-    const CACHE = 'zombie-v1';
-    const FILES = [
-      '/',
-      '/index.html',
-      '/manifest.json',
-      '/icon-192.png',
-      '/icon-512.png'
-    ];
+const CACHE = 'zombie-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/1png.png',           // <-- بدلنا
+  '/2png2.png',          // <-- بدلنا  
+  '/IMG-20260820-WA354...'  // <-- زيدنا الخريطة
+];
 
-    self.addEventListener('install', e => {
-      e.waitUntil(
-        caches.open(CACHE).then(cache => cache.addAll(FILES))
-      );
-    });
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
 
-    self.addEventListener('fetch', e => {
-      e.respondWith(
-        caches.match(e.request).then(res => res || fetch(e.request))
-      );
-    });
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
